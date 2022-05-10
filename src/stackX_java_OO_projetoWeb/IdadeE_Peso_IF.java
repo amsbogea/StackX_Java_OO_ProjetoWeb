75a2af5950573b1c14db199bc14c0dc36e0bc3d5
Alex Miranda <alex_miranda@outlook.com.br>
2022-05-10 13:27:54 -0300

    Idade e peso (IF)

---
+++ src/stackX_java_OO_projetoWeb/IdadeE_Peso_IF.java
@@ -0,0 +1,39 @@
+package stackX_java_OO_projetoWeb;
+
+import java.util.Scanner;
+
+public class IdadeE_Peso_IF {
+
+	public static void main(String[] args) {
+		int idade;
+		double peso;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Digito a idade");
+		idade = ler.nextInt();
+		
+		System.out.println("Digite o peso");
+		peso = ler.nextDouble();
+		
+		if(idade <20 && peso <= 60) {
+			System.out.println("Grupo de Risco 9");
+		}else if(idade < 20 && peso > 60 && peso <=90) {
+			System.out.println("Grupo de Risco 8");
+		}else if(idade < 20 && peso >= 90) {
+			System.out.println("Grupo de Risco 7");
+		}else if(idade >= 20 && idade <= 50 && peso <= 60) {
+			System.out.println("Grupo de Risco 6");
+		}else if(idade >= 20 && idade <= 50 && peso > 60 && peso <=90) {
+			System.out.println("Grupo de Risco 5");
+		}else if(idade >= 20 && idade <= 50 && peso >= 90) {
+			System.out.println("Grupo de Risco 4");
+		}else if(idade >50 && peso <= 60) {
+			System.out.println("Grupo de Risco 3");
+		}else if(idade > 50 && peso > 60 && peso <=90) {
+			System.out.println("Grupo de Risco 2");
+		}else if(idade > 50 && peso >= 90) {
+			System.out.println("Grupo de Risco 1");
+		}
+	}
+
+}
