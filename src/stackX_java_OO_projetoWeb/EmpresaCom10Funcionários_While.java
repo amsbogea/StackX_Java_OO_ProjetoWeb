47deaedb844b43973d914160915c53e878ce0b05
Alex Miranda <alex_miranda@outlook.com.br>
2022-05-11 17:23:01 -0300

    empresa com 10 funcionários mudanças

--- src/stackX_java_OO_projetoWeb/EmpresaCom10Funcionários_While.java
+++ src/stackX_java_OO_projetoWeb/EmpresaCom10Funcionários_While.java
@@ -8,8 +8,10 @@ public class EmpresaCom10Funcionários_While {
 		String categoria = "";
 		String turno = "";
 		double salMinimo = 450.00;
-		double horaTrabalhada = 0;
-
+		double salPorHora = 0;
+		int horasTrabalhadas = 44;
+		int totalHorasTrabalhadas;
+		double salLiquido;
 		Scanner in = new Scanner(System.in);
 
 		while (!categoria.equals("O") && !categoria.equals("G")) {
@@ -25,17 +27,21 @@ public class EmpresaCom10Funcionários_While {
 		in.close();
 		
 		if(categoria.equals("G") && turno.equals("N")) {
-			horaTrabalhada =  + (salMinimo *0.18);
-			System.out.println(horaTrabalhada);
+			salPorHora =  + (salMinimo *0.18); //salario por hora
+			salLiquido = horasTrabalhadas * salPorHora; //total de salario
+			System.out.println("Categoria GERENTE, turno NOTURNO, salário = R$" + salPorHora + " reais por hora trabalhada! Foram trabalhadas " +horasTrabalhadas+ " horas, total a receber R$" +  salLiquido);
 		}else if(categoria.equals("G") && turno.equals("M") || turno.equals("V")) {
-			horaTrabalhada =  + (salMinimo *0.15);
-			System.out.println(horaTrabalhada);
+			salPorHora =  + (salMinimo *0.15);
+			salLiquido = horasTrabalhadas * salPorHora; //total de salario
+			System.out.println("Categoria GERENTE, turno DIURNO, salário = R$" + salPorHora + " reais por hora trabalhada! Foram trabalhadas " +horasTrabalhadas+ " horas, total a receber R$" +  salLiquido);
 		}else if(categoria.equals("O") && turno.equals("N")) {
-			horaTrabalhada =  + (salMinimo *0.13);
-			System.out.println(horaTrabalhada);
+			salPorHora =  + (salMinimo *0.13);
+			salLiquido = horasTrabalhadas * salPorHora; //total de salario
+			System.out.println("Categoria OPERACINAL, turno NOTURNO, salário = R$" + salPorHora + " reais por hora trabalhada! Foram trabalhadas " +horasTrabalhadas+ " horas, total a receber R$" +  salLiquido);
 		}else if(categoria.equals("O") && turno.equals("M") || turno.equals("V")) {
-			horaTrabalhada =  + (salMinimo *0.10);
-			System.out.println(horaTrabalhada);
+			salPorHora =  + (salMinimo *0.10);
+			salLiquido = horasTrabalhadas * salPorHora; //total de salario
+			System.out.println("Categoria OPERACINAL, turno DIURNO, salário = R$" + salPorHora + " reais por hora trabalhada! Foram trabalhadas " +horasTrabalhadas+ " horas, total a receber R$" +  salLiquido);
 		}
 	}
 }	
