27be0eabae5704e8fac2dc3a14ddbef23bc864ec
Alex Miranda <alex_miranda@outlook.com.br>
2022-05-13 15:38:09 -0300

    EstruturasCondicionaisEDeRepetição_AumentoSalarial_FOR.

---
+++ src/stackX_java_OO_projetoWeb/EstruturasCondicionaisEDeRepetição_AumentoSalarial_FOR.java
@@ -0,0 +1,10 @@
+package stackX_java_OO_projetoWeb;
+
+public class EstruturasCondicionaisEDeRepetição_AumentoSalarial_FOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
