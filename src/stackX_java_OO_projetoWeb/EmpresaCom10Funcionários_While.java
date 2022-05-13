8fb14dd762c6020a188c0b34a457634bdff974fb
Alex Miranda <alex_miranda@outlook.com.br>
2022-05-13 09:50:05 -0300

    Empresa com 10 funcionários (While) Finalizado

--- src/stackX_java_OO_projetoWeb/EmpresaCom10Funcionários_While.java
+++ src/stackX_java_OO_projetoWeb/EmpresaCom10Funcionários_While.java
@@ -5,43 +5,84 @@ import java.util.Scanner;
 public class EmpresaCom10Funcionários_While {
 
 	public static void main(String[] args) {
-		String categoria = "";
-		String turno = "";
+		byte qtdFucnionario;
+		byte codigo;
+		String categoria;
+		String turno;
 		double salMinimo = 450.00;
 		double salPorHora = 0;
-		int horasTrabalhadas = 44;
-		int totalHorasTrabalhadas;
-		double salLiquido;
+		int horasTrabalhadas;
+		double salarioMes;
+		double auxilioAlimentacao;
 		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Digite a quantidade de funcionarios: ");
+		qtdFucnionario = in.nextByte();
+		
+		for (int i = qtdFucnionario; i > 0; i--) {
+			
+			categoria =" ";
+			turno = " ";
+			
+		System.out.print("Digite o código do fucnionário: ");
+			codigo = in.nextByte();
+			
+			in.nextLine();
 
+		System.out.print("Digite a categoria: ");
+		categoria = in.nextLine();
 		while (!categoria.equals("O") && !categoria.equals("G")) {
-			System.out.println("Digite a categoria");
-			categoria = in.nextLine();
+			
+			if (!categoria.equals("O") && !categoria.equals("G")) {
+				System.out.print("Categoria inválida, digite G para gerente ou O para operacional: ");
+				categoria = in.nextLine();
+			}
 		}
 
+		System.out.print("Digite o turno: ");
 		while (!turno.equals("M") && !turno.equals("V") && !turno.equals("N")) {
-			System.out.println("Digite o turno");
 			turno = in.nextLine();
+			if (!turno.equals("M") && !turno.equals("V") && !turno.equals("N")) {
+				System.out.print("Categoria inválida, digite M para Matutino ou V para Vespertino ou N para Noturno: ");
+			}
 		}
 		
-		in.close();
+		System.out.print("Digite a quantidade de horas trabalhadas do fucnionário: ");
+			horasTrabalhadas = in.nextInt();
 		
-		if(categoria.equals("G") && turno.equals("N")) {
-			salPorHora =  + (salMinimo *0.18); //salario por hora
-			salLiquido = horasTrabalhadas * salPorHora; //total de salario
-			System.out.println("Categoria GERENTE, turno NOTURNO, salário = R$" + salPorHora + " reais por hora trabalhada! Foram trabalhadas " +horasTrabalhadas+ " horas, total a receber R$" +  salLiquido);
-		}else if(categoria.equals("G") && turno.equals("M") || turno.equals("V")) {
-			salPorHora =  + (salMinimo *0.15);
-			salLiquido = horasTrabalhadas * salPorHora; //total de salario
-			System.out.println("Categoria GERENTE, turno DIURNO, salário = R$" + salPorHora + " reais por hora trabalhada! Foram trabalhadas " +horasTrabalhadas+ " horas, total a receber R$" +  salLiquido);
-		}else if(categoria.equals("O") && turno.equals("N")) {
-			salPorHora =  + (salMinimo *0.13);
-			salLiquido = horasTrabalhadas * salPorHora; //total de salario
-			System.out.println("Categoria OPERACINAL, turno NOTURNO, salário = R$" + salPorHora + " reais por hora trabalhada! Foram trabalhadas " +horasTrabalhadas+ " horas, total a receber R$" +  salLiquido);
-		}else if(categoria.equals("O") && turno.equals("M") || turno.equals("V")) {
-			salPorHora =  + (salMinimo *0.10);
-			salLiquido = horasTrabalhadas * salPorHora; //total de salario
-			System.out.println("Categoria OPERACINAL, turno DIURNO, salário = R$" + salPorHora + " reais por hora trabalhada! Foram trabalhadas " +horasTrabalhadas+ " horas, total a receber R$" +  salLiquido);
+		if (categoria.equals("G")) {
+			if (turno.endsWith("N")) {
+				salPorHora = +(salMinimo * 0.18);
+			} else
+				salPorHora = +(salMinimo * 0.15);
+		} else if (categoria.equals("O")) {
+			if (turno.endsWith("N")) {
+				salPorHora = +(salMinimo * 0.13);
+			} else
+				salPorHora = +(salMinimo * 0.10);
 		}
+
+		salarioMes = salPorHora * horasTrabalhadas;
+
+		if (salarioMes <= 300) {
+			auxilioAlimentacao = salarioMes * 0.20;
+		} else if (salarioMes > 300 && salarioMes <= 600) {
+			auxilioAlimentacao = salarioMes * 0.15;
+		} else {
+			auxilioAlimentacao = salarioMes * 0.05;
+		}
+		
+		System.out.println("--------------------------------------------------------------------------------");
+		System.out.print(" codigo do funcionário: " + codigo + "\n");
+		System.out.print(" número de horas trabalhadas: "+horasTrabalhadas+"\n");
+		System.out.print(" valor da hora trabalhada: "+ salPorHora + "\n");
+		System.out.print(" salário inicial: "+salarioMes+"\n");
+		System.out.print(" auxílio alimentação: "+auxilioAlimentacao+"\n");
+		System.out.print(" salário final: (salário + auxílio alimentação: "+ (salarioMes+auxilioAlimentacao));
+		System.out.println("\n--------------------------------------------------------------------------------");
+		System.out.println();
+
+		}
+
 	}
-}	
+}
